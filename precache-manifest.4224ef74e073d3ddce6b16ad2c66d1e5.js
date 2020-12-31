5dd46bd119c52b2359cde6fbf346b9b5368234da
nilesh9586 <nilesh16mcg34@gmail.com>
2020-12-31 17:08:16 +0530

    Updates

---
+++ precache-manifest.4224ef74e073d3ddce6b16ad2c66d1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a769434ae0c01e1beeb",
+    "url": "/newyear2021/static/css/main.f0232132.chunk.css"
+  },
+  {
+    "revision": "9a769434ae0c01e1beeb",
+    "url": "/newyear2021/static/js/main.feacdc9a.chunk.js"
+  },
+  {
+    "revision": "abb9bcc2cb15e15e73ee",
+    "url": "/newyear2021/static/js/runtime~main.cd155f45.js"
+  },
+  {
+    "revision": "4400130fabe57253dcde",
+    "url": "/newyear2021/static/js/2.5249f8be.chunk.js"
+  },
+  {
+    "revision": "0257a4af3a31697ac9430301c4588b05",
+    "url": "/newyear2021/static/media/images3.0257a4af.jpg"
+  },
+  {
+    "revision": "f01e8036a2c85c236cd057d44bbf8046",
+    "url": "/newyear2021/index.html"
+  }
+];
